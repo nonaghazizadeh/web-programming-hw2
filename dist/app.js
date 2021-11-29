336b4039c619e429f5f7946f180b5ec460c4a898
ShayanEmzed <shayan80sama@gmail.com>
2021-11-29 11:12:31 +0330

    add changing color function

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-$("#yes-button").click(function() {
-    $(".toast").toast('show');
-});
-function socialBtnOpen(user) {
-    window.open(`https://github.com/$[user]`, `_blank`);
-}
---
+++ dist/index.js
@@ -0,0 +1,19 @@
+// $("#yes-button").click(function () {
+//     $(".toast").toast('show');
+// });
+function socialBtnOpen(user) {
+    window.open(`https://github.com/$[user]`, `_blank`);
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function generate() {
+    console.log('hi')
+    var arr = []
+    for (let i = 0; i < 6; i++) {
+        arr[i] = randomIntFromInterval(1, 256);
+    }
+    var gradient = "linear-gradient(to left , rgba(" + arr[0] + "," + arr[1] + " , " + arr[2] + ", 0.52), rgba(" + arr[3] + ", " + arr[4] + ", " + arr[5] + ", 0.73))";
+    document.getElementById("cp").style.backgroundImage = gradient
+}
