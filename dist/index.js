d49b37eac70a6878df2bebb2058ad25cb03dd74a
ShayanEmzed <shayan80sama@gmail.com>
2021-11-29 22:28:24 +0330

    change home bg color randomly

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,7 @@ function generate() {
     for (let i = 0; i < 6; i++) {
         arr[i] = randomIntFromInterval(1, 256);
     }
-    var gradient = "linear-gradient(to left , rgba(" + arr[0] + "," + arr[1] + " , " + arr[2] + ", 0.52), rgba(" + arr[3] + ", " + arr[4] + ", " + arr[5] + ", 0.73))";
-    document.getElementById("cp").style.backgroundImage = gradient
+    var gradient = "linear-gradient(to left , rgba(" + arr[0] + "," + arr[1] + " , " + arr[2] + ", 0.52), rgba(" + arr[3] + ", " + arr[4] + ", " + arr[5] + ", 0.73)),";
+    var some_fancy_image = '../src/images/1856310.jpg'
+    document.getElementById("cp").style.backgroundImage = gradient + 'url(' + some_fancy_image + ')';
 }
